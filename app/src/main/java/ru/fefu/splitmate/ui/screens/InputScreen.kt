eb1f5b79f40ed914ebc065ba632896425c3ab92d
Maria Zinchenko <maaz1nchenko@gmail.com>
2026-01-29 21:01:12 +1000

    Update InputScreen.kt

--- app/src/main/java/ru/fefu/splitmate/ui/screens/InputScreen.kt
+++ app/src/main/java/ru/fefu/splitmate/ui/screens/InputScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.unit.dp
-import com.example.splitmate.ui.viewmodels.SharedViewModel
+
+import com.example.splitmate.ui.viewmodels.SharedViewModel 
 
 @Composable
 fun InputScreen(
